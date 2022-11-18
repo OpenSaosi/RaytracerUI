147339daf971e8f8ba7a95ab4cf07005434c5d41
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2022-11-18 22:07:23 +0000

    simplified example

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
 <v-app>
     <!--<v-app-bar app color="#FFFFFF" flat style="height:60px;border-bottom:1px solid #EEEEEE">
-        <div class="d-flex alig
+        <div class="d-flex align-center">
+            <
