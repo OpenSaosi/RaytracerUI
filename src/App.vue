df46f9bbcecb0bacd7944e7146a7fa8ee590506e
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2022-11-18 20:23:58 +0000

    Refactor variable name

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
 <template>
 <v-app>
-    <!--<v-app-bar app color="#FFFFFF" flat style="height:60px;border-bottom:1px solid #EEEEEE
+    <!--<v-app-bar app color="#FFFFFF" flat style="height:60px;border-bottom:1px solid #EEEEEE">
+        <div class="d-flex alig
