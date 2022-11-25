9a8a45352f02479abadd1a50d27aedcdeaeaa7ac
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2022-11-25 12:42:56 +0000

    adding debug_funk2.sh

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,4 @@
 <v-app>
     <!--<v-app-bar app color="#FFFFFF" flat style="height:60px;border-bottom:1px solid #EEEEEE">
         <div class="d-flex align-center">
-            <
+            <v-img alt="Vuetify Logo
