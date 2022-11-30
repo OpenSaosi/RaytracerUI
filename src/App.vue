7f8aa50460d7029e9e04495265f0d1d664269aec
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2022-11-30 22:08:23 +0000

    ARM: mm: update __v7_setup() to the new LoUIS cache maintenance API

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,4 @@
 <v-app>
     <!--<v-app-bar app color="#FFFFFF" flat style="height:60px;border-bottom:1px solid #EEEEEE">
         <div class="d-flex align-center">
-            <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="./assets/nonmql_logo.
+            <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="./assets/nonmql_logo.png" transition="scale-transition" w
