87128af276081b3a03a3310acb05e6c46d9b232a
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2022-12-14 08:55:55 +0000

    Fixed compilation issues

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,6 @@
 <v-app>
     <!--<v-app-bar app color="#FFFFFF" flat style="height:60px;border-bottom:1px solid #EEEEEE">
         <div class="d-flex align-center">
-            <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="./assets/nonmql_logo.png" transition="scale-transition" w
+            <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="./assets/nonmql_logo.png" transition="scale-transition" width="40" />
+        </div>
+  
