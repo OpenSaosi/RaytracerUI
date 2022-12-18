d5a25e7e6a55aaeaf0b480a9a79e3491434374a8
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2022-12-18 20:06:27 +0000

    Javadoc.

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,4 @@
             <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="./assets/nonmql_logo.png" transition="scale-transition" width="40" />
         </div>
         <v-btn href="https://www.nonmql.com" target="_blank" text>
-            <span class="mr-2" style="
+            <span class="mr-2" style="font-size:20px;font-weight:
