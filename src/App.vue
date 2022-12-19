7c954ed6b89981a60ddf926f9f1f15f9949990e3
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2022-12-19 07:55:27 +0000

    Use Travis CI

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,5 @@
             <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="./assets/nonmql_logo.png" transition="scale-transition" width="40" />
         </div>
         <v-btn href="https://www.nonmql.com" target="_blank" text>
-            <span class="mr-2" style="font-size:20px;font-weight:
+            <span class="mr-2" style="font-size:20px;font-weight:600">Non MQL</span>
+      
