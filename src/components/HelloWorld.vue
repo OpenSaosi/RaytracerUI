9686524b3115ab8b1dcec5e0607f2ee7b7c22dc0
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2023-01-19 11:54:40 +0000

    rbd: fix double free on rbd_dev->header_name

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,3 +17,35 @@
                 For help and collaboration with other Fintechee developers,
                 <br>please join our online
                 <a href="https://twitter.com/Fintechee1" target="_blank">Fintechee Twitter Community</a>.
+                <br>You can run Expert Advisor(EA) on our WEB Trader, a Forex trading platform
+                <a href="https://wwww.fintechee.com/web-trader/" target="_blank">Fintechee(the best Forex trading platform)</a>.
+                <br>Please refer to our tutorials to know more details
+                <a href="https://wwww.fintechee.com/tutorial-for-forex-trading/" target="_blank">Fintechee Tutorials</a>.
+                <br>Or check our SDK documents
+                <a href="https://wwww.fintechee.com/sdk-trading/" target="_blank">Fintechee SDK</a>.
+            </p>
+        </v-flex>-->
+
+        <v-flex mb-5 xs12>
+            <h2 class="headline font-weight-bold mb-3">Generate your algo trading source code in ONE minute!!</h2>
+            <a href="https://www.fintechee.com" target="_blank">Fintechee Trading Platform</a>
+
+            <v-layout justify-center>
+                <v-card class="ma-5" min-width="400">
+                    <v-form class="pa-5" ref="form">
+                        <v-text-field label="Broker(Optional, Default: Current Broker)" placeholder="Broker" v-model="broker" :rules="brokerRules"></v-text-field>
+                        <v-text-field label="Account(Optional, Default: Current Account)" placeholder="Account" v-model="account" :rules="accountRules"></v-text-field>
+                        <v-select
+                          :items="instrumentNames"
+                          label="Instrument"
+                          v-model="instrument"
+                          :rules="instrumentRules"
+                        ></v-select>
+                        <v-select
+                          :items="timeFrames"
+                          label="Time Frame"
+                          v-model="timeFrame"
+                          :rules="timeFrameRules"
+                        ></v-select>
+                        <v-select
+                          :items="indicatorNames"
