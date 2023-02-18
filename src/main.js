69472b5135d55a17fd71ad780853de2eaf96424c
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2023-02-18 13:23:44 +0000

    CSS fixes.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import 'material-design-icons-iconfont
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue
