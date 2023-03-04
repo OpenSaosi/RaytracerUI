4d8ef4985893a0e6c0c42500b0c55304fa3d8750
OpenSaosi <121471460+OpenSaosi@users.noreply.github.com>
2023-03-04 06:15:18 +0000

    BPMSPL-44 : Improve metadata tab

--- src/router/index.js
+++ src/router/index.js
@@ -16,4 +16,4 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackC
+    component: () => import(/* webpackChunkName: "about" */ '../views/Abo
